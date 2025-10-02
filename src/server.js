6d3fda0513c2dbde4ff229b94708b494e6039a57
kev <moralesperezkevin7@gmail.com>
2025-10-02 09:03:43 -0600

    feat (server.js): Add instance of server

---
+++ src/server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const PORT = 3000;
+
+const server = http.createServer();
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
