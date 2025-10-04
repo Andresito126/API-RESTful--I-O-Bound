3bbb454e88a13b2a6216dd874630f60f3859b6bc
Andresito126 <creed126ax@gmail.com>
2025-10-04 16:30:20 -0600

    feat: librery removed to use more io bound than cpu

--- src/controllers/UrlImagesController.js
+++ src/controllers/UrlImagesController.js
@@ -16,16 +16,25 @@ export const convertUrlsToPDFController = async (req, res) => {
 
     urls = JSON.parse(body).urls || [];
     if (!Array.isArray(urls) || urls.length === 0) {
-      throw new Error("No se proporcionó un array");
+      throw new Error("No se proporcionó un array bien");
     }
   } catch (e) {
     res.writeHead(400, { "Content-Type": "application/json" });
-    return res.end(JSON.stringify({ error: "datos inválidos o faltantes." }));
+    return res.end(JSON.stringify({ error: "datos invalidos o faltantes." }));
   }
 
   try {
-    // usar el service para descargar imágenes
+    // usar el service para descargar las imgs 
     const uploadsDir = path.resolve('./src/uploads');
+
+    if (fs.existsSync(uploadsDir)) {
+      fs.rmSync(uploadsDir, { recursive: true, force: true });
+      console.log(`Carpeta '${uploadsDir}' eliminada.`);
+    }
+    
+    fs.mkdirSync(uploadsDir, { recursive: true });
+    console.log(`Carpeta '${uploadsDir}' creada.`);
+
     const downloadedFiles = await UrlImageService.createFolderAndImages(urls,uploadsDir);
 
     if (downloadedFiles.length === 0) {
@@ -41,7 +50,7 @@ export const convertUrlsToPDFController = async (req, res) => {
 
     const outputPath = await processor.process();
 
-    // 4. da el pdf
+    // da el pdf
     res.writeHead(200, {
       "Content-Type": "application/pdf",
       "Content-Disposition": `attachment; filename="${path.basename(outputPath)}"`
@@ -52,19 +61,17 @@ export const convertUrlsToPDFController = async (req, res) => {
 
     // limpia los archivos
     res.on("finish", async () => {
-      try {
-        await fs.promises.unlink(outputPath);
-      } catch (err) {
-        console.error("error en borrar el pdf:", err);
-      }
+        const filesToDelete = [
+        outputPath,
+        ...downloadedFiles.map(file => file.path)
+    ];
 
-      for (const file of downloadedFiles) {
-        try {
-          await fs.promises.unlink(file.path);
-        } catch (err) {
-          console.error(`error e borrar la imagen ${file.path}:`, err);
-        }
-      }
+    // eecuta el borrado de todos los archivos en paralelo
+    await Promise.all(filesToDelete.map(filePath => 
+        fs.promises.unlink(filePath).catch(err => {
+        console.error(`Error al borrar archivo ${filePath}:`, err.message);
+        })
+    ));
     });
 
   } catch (err) {
--- src/services/UrlImageService.js
+++ src/services/UrlImageService.js
@@ -2,12 +2,25 @@ import fs from "fs";
 import path from "path";
 import http from "http";
 import https from "https";
-import sharp from "sharp";
-
 
 export class UrlImageService {
+  static getFileExtension(url, contentType) {
+    // checa desde contet type
+    if (contentType && contentType.startsWith("image/")) {
+      const mimeType = contentType.split("/")[1];
+      if (["png", "jpeg", "jpg"].includes(mimeType)) {
+        return mimeType;
+      }
+    }
+    // verifica desde la url
+    const ext = path.extname(url).toLowerCase();
+    if ([".png", ".jpg", ".jpeg"].includes(ext)) {
+      return ext.substring(1);
+    }
+    return null;
+  }
 
-    static isValidHttpUrl(url) {
+  static isValidHttpUrl(url) {
     try {
       const u = new URL(/^https?:\/\//i.test(url) ? url : "http://" + url);
       return u.protocol === "http:" || u.protocol === "https:";
@@ -16,78 +29,82 @@ export class UrlImageService {
     }
   }
 
-
   static async downloadImage(url, uploadsDir) {
-    
-    // siempre se guardara en formato JPG
-    const filename = path.join(uploadsDir, `${Date.now()}-${Math.floor(Math.random()*1000)}.jpg`);
     const client = url.startsWith("https") ? https : http;
 
-    return new Promise((resolve, reject) => {  
+    return new Promise((resolve, reject) => {
 
-      //buffer en memoria 
-      const chunks = [];
-      client.get(url, {  headers: {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}
-        }, (response) => {
+      client.get(url, {  headers: {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0"}}, (response) => {
           if (response.statusCode !== 200) {
           reject(new Error(`Error al descargar: ${response.statusCode}`));
           return;
         }
-        
-        // aca se acumulan los datos en memoria
-        // para luego procesarlos con sharp
-        response.on("data", (chunk) => chunks.push(chunk));
+            // valida y obtiene la extension
+            const contentType = response.headers["content-type"] || "";
+            const fileExt = this.getFileExtension(url, contentType);
 
-        // esto es para cuando termina la descarga
-        response.on("end", async () => {
-        try {
-          // une todos los fragmentos en uno solo, el buffer
-          const buffer = Buffer.concat(chunks);
-          // se procesa primero y luego se convierte a jpg antes de guardar
-          // y devuelve el nombre del archivo
-          await sharp(buffer).jpeg().toFile(filename);
-          resolve({
-              path: filename,
-              mimetype: "image/jpeg",
-              originalname: path.basename(filename)
+            if (!fileExt) {
+              return reject(
+                new Error("tipo de imagen no soportado o indeterminable")
+              );
+            }
+
+            //crear el stream de escritura y nombrambiento
+            const filename = path.join(
+              uploadsDir,`${Date.now()}-${Math.floor(Math.random() * 1000)}.${fileExt}`
+            );
+            const fileStream = fs.createWriteStream(filename);
+
+            // pipe del stream de red a disco
+            response.pipe(fileStream);
+
+            //eventos del stream de disco
+
+            fileStream.on("finish", () => {
+              resolve({
+                path: filename,
+                mimetype: contentType,
+                originalname: path.basename(filename),
+              });
             });
-        } catch (err) {
-          reject(err);
-        }
-      });
-    }).on("error", (err) => reject(err));
-  });
 
+            fileStream.on("error", (err) => {
+              fs.unlink(filename, () => {}); 
+              reject(new Error(`Error de escritura en disco: ${err.message}`));
+            });
+            
+            response.on("error", (err) => {
+              fileStream.close();
+              reject(
+                new Error(`Error de red durante la descarga: ${err.message}`)
+              );
+            });
+          }
+        )
+        .on("error", (err) => {
+          reject(new Error(`Error de conexión al URL: ${err.message}`));
+        });
+    });
   }
 
   static async createFolderAndImages(urls, uploadsDir) {
-  // si existe la carpeta, se borra y se crea de nuevo
-  if (fs.existsSync(uploadsDir)) {
-    fs.rmSync(uploadsDir, { recursive: true, force: true });
-    console.log(`Carpeta '${uploadsDir}' eliminada.`);
+    
+    const downloadPromises = urls.map((url) => {
+      if (!this.isValidHttpUrl(url)) {
+        console.error("url invalida:", url);
+        return Promise.resolve(null);
+      }
+      // Llama a la función de descarga I/O Bound
+      return this.downloadImage(url, uploadsDir).catch((err) => {
+        console.error(`error descargando ${url}: ${err.message}`);
+        return null;
+      });
+    });
+
+    // promise.all las ejecuta concurrentemente
+    const results = await Promise.all(downloadPromises);
+
+    // retorna solo los archivos que se descargaron con éxito
+    return results.filter((file) => file !== null);
   }
-
-  fs.mkdirSync(uploadsDir, { recursive: true });
-  console.log(`Carpeta '${uploadsDir}' creada.`);
-
-  const results = [];
-  for (let url of urls) {
-
-    if (!this.isValidHttpUrl(url)) {
-    console.error("URL inválida, se omite:", url);
-    continue;
-  }
-
-    try {
-
-      // descarga y convierte cada imagen
-      const file = await this.downloadImage(url, uploadsDir);
-      results.push(file);
-    } catch (err) {
-      console.error("Error descargando", url, err);
-    }
-  }
-  return results;
-}
-
 }
